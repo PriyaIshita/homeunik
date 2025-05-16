2d82b48ccea3489837c1782b87d7945c783b594b
PriyaIshita <priyaabugade@gmail.com>
2025-05-16 20:37:28 +0530

    Site section css changes

--- src/Blog.js
+++ src/Blog.js
@@ -11,41 +11,21 @@ const Blog = () => {
       ];
     
   return (
-    <div style={{ marginLeft:'20px', marginRight:'20px',paddingTop: '10px', paddingBottom:'70px', marginBottom:'160px' }}>
-    <p style={{ textAlign: 'center', letterSpacing: '2px', fontFamily: 'Kanchenjunga, sans-serif', textTransform: 'uppercase', fontSize: '10px', marginBottom: '10px', marginTop: '20px' }}>
-      News && Blog
-    </p>
-    <h1 style={{ color: '#2f3d7e', fontWeight: 'bold', fontFamily: 'Kanchenjunga, sans-serif', fontSize: '40px', textAlign: 'center' }}>
-    Read Our Latest News
-    </h1>
+    <div className='blogDiv'>
+    <p className='blogDiv-p'> News && Blog </p>
+    <h1 className='blogDiv-h1'> Read Our Latest News </h1>
   
-    <div style={{ display: 'flex', gap: '10px', margin: '40px 140px 0 140px' }}>
+    <div className='blogFirstDiv'>
       {propertyTypes.map((item, index) => (
-        <div key={index} style={{
-          width: '320px',
-          height: '100px',
-          border: '1px solid #edf0ff',
-          borderRadius: '14px',
-          textAlign: 'left',
-          backgroundColor: '#fff',
-          
-        }}>
-          <div style={{ position: 'relative',  }}>
+        <div key={index}  className='blogFirstDiv-map'>
+          <div className='blogSecondDiv'>
             <img
               src={item.icon}
               alt={item.title}
-              style={{
-                width: '300px',
-                height: '180px',
-                objectFit: 'cover',
-                borderTopLeftRadius:'14px',
-                borderBottomRightRadius:'14px',
-                borderBottomLeftRadius:'14px'
-              }}
-            />
+                className='blogSecondDiv-img'/>
           </div>
-          <h3 style={{ margin: '10px', fontSize: '18px', color:'#b5b4b4' }}>{item.title}</h3>
-          <p style={{margin: '0 10px 5px 10px', fontSize: '13px', color: '#555', fontWeight: 'bold' }}>{item.title1}</p>
+          <h3  className='blogSecondDiv-h3'>{item.title}</h3>
+          <p className='blogSecondDiv-p'>{item.title1}</p>
         </div>
       ))}
     </div>
--- src/Site.js
+++ src/Site.js
@@ -12,63 +12,21 @@ const Site = () => {
       ];
     
   return (
-    <div style={{ backgroundColor: '#f6f8ff', paddingTop:'20px', paddingBottom:'70px' }}>
-    <p style={{textAlign: 'center', letterSpacing: '2px', fontFamily: 'Kanchenjunga, sans-serif', textTransform: 'uppercase', fontSize: '10px', marginBottom: '10px', marginTop: '20px' }}>
-        lOCATION
-    </p>
-    <h1 style={{ color: '#2f3d7e', fontWeight: 'bold', fontFamily: 'Kanchenjunga, sans-serif', fontSize: '40px', textAlign: 'center' }}>
-        Properties By Cities
-    </h1>
+    <div className='siteDiv'>
+    <p className='siteDiv-p' >lOCATION</p>
+    <h1 className='siteDiv-h1'> Properties By Cities</h1>
   
-    <div style={{  marginTop:'40px', gap:'20px', left:'280px',display: 'flex', justifyContent: 'space-between', position: 'relative', width: '800px', height: '260px', borderRadius:'14px'  }}>
+    <div className='siteFirstDiv'>
     {propertyTypes.map((item, index) => (
-  <div key={index} style={{
-    width: '250px',
-    height: '250px',
-    border: '1px solid #edf0ff',
-    borderRadius: '14px',
-    backgroundColor: '#fff',
-    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.05)',
-    overflow: 'hidden',
-    position: 'relative',
-  }}>
-    <div style={{ position: 'relative', width: '100%', height: '260px' }}>
-      <img
-        src={item.icon}
-        alt={item.title}
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          gap:'10px'
-        }}
-      />
-      <div style={{
-        position: 'absolute',
-        top: '10px',
-        left: '10px',
-        color: 'white',
-        padding: '6px 10px',
-        borderRadius: '6px',
-      }}>
-        <h3 style={{ margin: '0', fontSize: '16px' }}>{item.title}</h3>
-        <p style={{ margin: '0', fontSize: '12px' }}>{item.title1}</p>
+  <div key={index} className='siteFirstDiv-map'>
+  <div className='sittSecondDiv'>
+      <img src={item.icon} alt={item.title} className='sittSecondDiv-img'/>
+      <div className='siteThirdDiv'>
+      <h3 className='siteThirdDiv-h3'>{item.title}</h3>
+      <p className='siteThirdDiv-p'>{item.title1}</p>
       </div>
       {index === 0 && (
-      <button style={{
-        position: 'absolute',
-        bottom: '10px',
-        left: '10px',
-        backgroundColor: '#ed7d3a',
-        borderRadius: '6px',
-        padding: '6px 12px',
-        fontSize: '13px',
-        color: 'white',
-        border: 'none',
-        cursor: 'pointer'
-      }}>
-        See More
-      </button>
+      <button className='siteDiv-button'> See More </button>
     )}
     </div>
   </div>
